9ae27b03c29c91b9d66efeb75fa801e8324f58fa
Pablo Coronel <pablomanuelcoronel@gmail.com>
2018-06-24 19:57:09 -0300

    hecho componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './welcome.js';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+
+    this.state = {
+      titulo : "Hola mundo desde estado"
+    }
+
+  }
+
+  cambiarTitulo = () => {
+    this.setState({
+      titulo : "Nuevo titulo"
+    })
+  }
+
   render() {
+    const nombres = ["Pablo", "Pedro", "Laura"];
+
     return (
-      <h1>Hola mundo</h1>
+      <div>
+        {nombres.map((nombre, index) => <Welcome key={index} name={nombre} />)}
+
+        <h1>{this.state.titulo}</h1>
+
+        <button onClick={this.cambiarTitulo}>Cambiar el titulo del estado</button>
+      </div>
     );
   }
 }
---
+++ src/welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Welcome = (props) => <h1>Hola {props.name} Componente</h1>
+
+export default Welcome;
