27588d968767f53284e4a3f2cf87eba95c2d3562
Pablo Coronel <pablomanuelcoronel@gmail.com>
2018-06-24 22:52:54 -0300

    hecho ejemplo de axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hola mundo</h1>
-    );
-  }
+	constructor(props){
+		super(props);
+
+		// Definir el estado inicial
+		this.state = {
+			nombres : []
+		}
+
+		axios.get("http://localhost:3001/names")
+			.then(response => {
+				// console.log(response)
+
+				// toma los datos del server
+				this.setState({
+					nombres : response.data
+				})
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hola mundo</h1>
+			
+				{/* mostrar el listado */}
+				<ul>
+					{this.state.nombres.map((nom, index) => 
+						<li key={index}>{nom}</li>
+					)}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
