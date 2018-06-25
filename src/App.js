599831c5a6d3ddbf1035f418edd61051d9dc4096
Pablo Coronel <pablomanuelcoronel@gmail.com>
2018-06-24 21:19:04 -0300

    hecho ejemplo de input y checkbox

--- src/App.js
+++ src/App.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hola mundo</h1>
-    );
-  }
+  	constructor(props){
+  		super(props);
+
+  		this.state = {
+  			name : "",
+  			terms : false
+  		}
+  	}
+
+
+	actualizarNombre = (event) => {
+		this.setState({
+			name : event.target.value
+		})
+	}
+
+	actualizarTerminos = (event) => {
+		this.setState({
+			terms : event.target.checked
+		})
+	}
+
+	verSaludo = () => {
+		if (this.state.terms === true) {
+			alert("Hola " + this.state.name);
+		}else{
+			alert("Hay que aceptar los términos");
+		}
+	}
+
+
+	render() {
+		return (
+			<div>
+				<h1>Hola mundo</h1>
+
+				<input type="text" value={this.state.name} onChange={this.actualizarNombre} />
+
+				<div>
+					<label>
+						<input type="checkbox" checked={this.state.terms} onClick={this.actualizarTerminos} /> Acepto los términos
+					</label>
+				</div>
+				<button onClick={this.verSaludo}>Ver saludo</button>
+			</div>
+		);
+	}
 }
 
 export default App;
